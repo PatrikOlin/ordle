f8b009efe70b69301d161b9458a6165d5124b091
Patrik Olin <enmejl@gmail.com>
2022-03-17 16:09:01 +0100

    add shareable eomoji results

--- src/EndScreen.svelte
+++ src/EndScreen.svelte
@@ -5,6 +5,18 @@
         if (status === "unsolved") return "Du förlorade!";
         if (status === "solved") return "Du vann!";
     };
+
+    const convertWordState = (wordState) => {
+        let emojiState = "";
+        wordState.split("").forEach((pos) => (emojiState += getEmoji(pos)));
+        return emojiState;
+    };
+
+    const getEmoji = (posState) => {
+        if (posState === "G") return String.fromCodePoint(0x1f7e9);
+        if (posState === "Y") return String.fromCodePoint(0x1f7e8);
+        if (posState === ".") return String.fromCodePoint(0x2b1b);
+    };
 </script>
 
 <main>
@@ -13,8 +25,16 @@
         <h3>Ordet var: <span>{session.word}</span></h3>
         <p>Det tog dig {session.numberOfGuesses} gissningar.</p>
         <p>För att spela igen, tryck F5.</p>
+        <br />
+        <h3>Dela satan</h3>
+        {#each session.guesses as guess}
+            <span class="result">{convertWordState(guess.wordState)}</span><br />
+        {/each}
     {/if}
 </main>
 
 <style>
+ .result {
+     letter-spacing: 2px;
+ }
 </style>
